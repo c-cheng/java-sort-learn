8144ae19faf61550616de67e8a4b415a58670c91
ccheng <public_void_test@163.com>
2020-10-31 18:46:35 +0800

    添加gitignore

--- src/test/java/top/ccheng/sort/SortTest.java
+++ src/test/java/top/ccheng/sort/SortTest.java
@@ -10,21 +10,21 @@ public class SortTest {
 
     @Test
     public void testAll() {
-        int[] array = RandomUtil.randomIntArray(100000, 10000);
+        int[] array = RandomUtil.randomIntArray(1000000, 10000);
 
         int[] javaArray = javaSort(array.clone());
-        int[] bubbleArray = new BubbleSort().testSortAsc(array.clone());
+//        int[] bubbleArray = new BubbleSort().testSortAsc(array.clone());
         int[] quickArray = new QuickSort().testSortAsc(array.clone());
-        int[] selectArray = new SelectSort().testSortAsc(array.clone());
-        int[] insertArray = new InsertSort().testSortAsc(array.clone());
+//        int[] selectArray = new SelectSort().testSortAsc(array.clone());
+//        int[] insertArray = new InsertSort().testSortAsc(array.clone());
         int[] shellArray = new ShellSort().testSortAsc(array.clone());
         int[] mergeArray = new MergeSort().testSortAsc(array.clone());
         int[] heapArray = new HeapSort().testSortAsc(array.clone());
 
-        System.out.println(Arrays.equals(javaArray, bubbleArray));
+//        System.out.println(Arrays.equals(javaArray, bubbleArray));
         System.out.println(Arrays.equals(javaArray, quickArray));
-        System.out.println(Arrays.equals(javaArray, selectArray));
-        System.out.println(Arrays.equals(javaArray, insertArray));
+//        System.out.println(Arrays.equals(javaArray, selectArray));
+//        System.out.println(Arrays.equals(javaArray, insertArray));
         System.out.println(Arrays.equals(javaArray, shellArray));
         System.out.println(Arrays.equals(javaArray, mergeArray));
         System.out.println(Arrays.equals(javaArray, heapArray));
